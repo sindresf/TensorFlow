497a6fca23ce93884e1959f182a80ba9f9f055e9
sindresf <sindresfjermestad@gmail.com>
2016-09-27 17:11:12 +0200

    started census tutorial

---
+++ contrib_learn_tuts/census_income/census_income.py
@@ -0,0 +1,7 @@
+import pandas as pd
+COLUMNS = ["age", "workclass", "fnlwgt", "education", "education_num",
+           "marital_status", "occupation", "relationship", "race", "gender",
+           "capital_gain", "capital_loss", "hours_per_week", "native_country",
+           "income_bracket"]
+df_train = pd.read_csv(train_file, names=COLUMNS, skipinitialspace=True)
+df_test = pd.read_csv(test_file, names=COLUMNS, skipinitialspace=True, skiprows=1)
