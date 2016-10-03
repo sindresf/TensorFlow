942ac52934bfe8c5f90bfa3db6d1f4576ec38023
sindresf <sindresfjermestad@gmail.com>
2016-10-03 10:41:19 +0200

    census income results in 1.0 accuracy :/ think its a model_dir problem...

--- contrib_learn_tuts/census_income/census_income.py
+++ contrib_learn_tuts/census_income/census_income.py
@@ -3,5 +3,96 @@ COLUMNS = ["age", "workclass", "fnlwgt", "education", "education_num",
            "marital_status", "occupation", "relationship", "race", "gender",
            "capital_gain", "capital_loss", "hours_per_week", "native_country",
            "income_bracket"]
+CATEGORICAL_COLUMNS = ["workclass", "education", "marital_status", "occupation",
+                       "relationship", "race", "gender", "native_country"]
+CONTINUOUS_COLUMNS = ["age", "education_num", "capital_gain", "capital_loss", "hours_per_week"]
+train_file = "adult.data.csv"
+test_file = "adult.test.csv"
 df_train = pd.read_csv(train_file, names=COLUMNS, skipinitialspace=True)
 df_test = pd.read_csv(test_file, names=COLUMNS, skipinitialspace=True, skiprows=1)
+
+# Adds the column after all the data pertaining to the label of each case
+LABEL_COLUMN = "label"
+df_train[LABEL_COLUMN] = (df_train["income_bracket"].apply(lambda x: ">50k" in x)).astype(int)
+df_test[LABEL_COLUMN] = (df_test["income_bracket"].apply(lambda x: ">50k" in x)).astype(int)
+
+import tensorflow as tf
+
+def input_fn(df):
+  # Creates a dictionary mapping from each continuous feature column name (k) to
+  # the values of that column stored in a constant Tensor.
+  continuous_cols = {k: tf.constant(df[k].values)
+                     for k in CONTINUOUS_COLUMNS}
+  # Creates a dictionary mapping from each categorical feature column name (k)
+  # to the values of that column stored in a tf.SparseTensor.
+  categorical_cols = {k: tf.SparseTensor(
+      indices=[[i, 0] for i in range(df[k].size)],
+      values=df[k].values,
+      shape=[df[k].size, 1])
+                      for k in CATEGORICAL_COLUMNS}
+  # Merges the two dictionaries into one.
+  feature_cols = dict(continuous_cols)
+  feature_cols.update(categorical_cols)
+  # Converts the label column into a constant Tensor.
+  label = tf.constant(df[LABEL_COLUMN].values)
+  # Returns the feature columns and the label.
+  return feature_cols, label
+
+def train_input_fn():
+    return input_fn(df_train)
+
+def eval_input_fn():
+    return input_fn(df_test)
+
+# Defining actual tensor column
+gender = tf.contrib.layers.sparse_column_with_keys(
+    column_name="gender", keys=["female", "male"]) # Explicitly states the keys to convert into integer IDs
+                                                   # Has to know the full set of feature values for this
+
+education = tf.contrib.layers.sparse_column_with_hash_bucket(
+    "education", hash_bucket_size=1000)            # Does the same for when you don't know all feature values
+                                                   # while training is under way as new values are encountered
+
+# And so on...
+race = tf.contrib.layers.sparse_column_with_keys(column_name="race", keys=[
+  "Amer-Indian-Eskimo", "Asian-Pac-Islander", "Black", "Other", "White"])
+marital_status = tf.contrib.layers.sparse_column_with_hash_bucket("marital_status", hash_bucket_size=100)
+relationship = tf.contrib.layers.sparse_column_with_hash_bucket("relationship", hash_bucket_size=100)
+workclass = tf.contrib.layers.sparse_column_with_hash_bucket("workclass", hash_bucket_size=100)
+occupation = tf.contrib.layers.sparse_column_with_hash_bucket("occupation", hash_bucket_size=1000)
+native_country = tf.contrib.layers.sparse_column_with_hash_bucket("native_country", hash_bucket_size=1000)
+
+# Just defines the columns to be continuous for the tensor graph
+age = tf.contrib.layers.real_valued_column("age")
+education_num = tf.contrib.layers.real_valued_column("education_num")
+capital_gain = tf.contrib.layers.real_valued_column("capital_gain")
+capital_loss = tf.contrib.layers.real_valued_column("capital_loss")
+hours_per_week = tf.contrib.layers.real_valued_column("hours_per_week")
+
+# Makes a continuous feature into a categorical one through putting every value between
+# the boundaries defined into one ID
+age_buckets = tf.contrib.layers.bucketized_column(age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+
+# Creates a meta cross column based on the combination of two base ones
+# Makes it possible for the model to learn connections it wouldn't otherwise
+education_x_occupation = tf.contrib.layers.crossed_column([education, occupation], hash_bucket_size=int(1e4))
+
+# Three column based meta cross
+# This does create possible LARGE feature value spaces and such
+age_buckets_x_race_x_occupation = tf.contrib.layers.crossed_column(
+  [age_buckets, race, occupation], hash_bucket_size=int(1e6))
+
+# all columns, inicluding cross columns are simply listed together and the model a-gogo
+m = tf.contrib.learn.LinearClassifier(feature_columns=[
+  gender, native_country, education, occupation, workclass, marital_status, race,
+  age_buckets, education_x_occupation, age_buckets_x_race_x_occupation],
+  model_dir="census_income")
+
+# a wrapper for the for-loop for training with range(len=200)
+m.fit(input_fn=train_input_fn,steps=200)
+
+# Evaluates how well the 200 steps trained model predicts each label
+results = m.evaluate(input_fn=eval_input_fn, steps=1)
+for key in sorted(results):
+    print ("%s: %s" % (key, results[key]))
+
